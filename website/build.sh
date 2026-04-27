7e4664ad71477f8ccf14b925a16909f01b9f2f8a
Rasmus Andersson <rasmus@notion.se>
2026-04-26 18:21:14 -0700

    website favicon

--- website/build.sh
+++ website/build.sh
@@ -9,4 +9,12 @@ test build/package-lock.json -nt build/package.json -a -f build/node_modules/.bi
     cd ..
 }
 
+if test \
+    _favicon/favicon-32.png -nt favicon.ico -o \
+    _favicon/favicon-128.png -nt favicon.ico
+then
+    echo "generate favicon.ico from" _favicon/*.png
+    magick _favicon/*.png favicon.ico || true
+fi
+
 build/node_modules/.bin/mkweb -config build/mkweb.js "$@"
