11b9aba73e923245aaccc6588db96bd5850fcca7
Rasmus Andersson <rasmus@notion.se>
2026-05-14 13:48:07 -0700

    simplify a few rosetta examples

--- examples/rosetta/run-all.sh
+++ examples/rosetta/run-all.sh
@@ -2,4 +2,9 @@
 set -eu
 
 cd "$(dirname "$0")/../.."
-python3 tools/test.py run --suite examples.rosetta --build-dir _build/macos-aarch64-debug --cc clang
+python3 tools/test.py run \
+    --suite examples.rosetta \
+    --build-dir _build/macos-aarch64-debug \
+    --cc clang \
+    --log-progress \
+    "$@"
